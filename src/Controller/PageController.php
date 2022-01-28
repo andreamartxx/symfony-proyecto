c2aaaa1af6cae5eb47778e24684daddc3785416a
Andrea <amartin.onda@gmail.com>
2022-01-28 10:40:10 +0100

    cambios28E

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -39,6 +39,25 @@ class PageController extends AbstractController
 
     }
 
+     /**
+     * 
+     * @Route("/contacto", name="contacto")
+     */
+    public function contacto(): Response{
+
+        return $this->render("contacto.html.twig");
+
+    }
+
+     /**
+     * 
+     * @Route("/galeria", name="galeria")
+     */
+    public function galeria(): Response{
+
+        return $this->render("galeria.html.twig");
+
+    }
  
 }
 
